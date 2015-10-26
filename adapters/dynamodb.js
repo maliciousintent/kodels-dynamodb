0925e73d535d2ae3bfaa534b9d78123d8d40bcd3
Luca Casartelli <luca@plasticpanda.com>
2015-10-26 13:27:51 +0100

    trim spaces

--- adapters/dynamodb.js
+++ adapters/dynamodb.js
@@ -1,4 +1,4 @@
-
+/* eslint-env node */
 var _ = require('lodash');
 var AWS = require('aws-sdk');
 var shortId = require('shortid');
@@ -7,11 +7,11 @@ var async = require('async');
 var assert = require('assert');
 var util = require('util');
 
-module.exports = DynamoDriver;
+
 function DynamoDriver(params) {
   params = params || {};
   params.apiVersion = '2012-08-10';
-  
+
   this.$params = params;
   this.$db = new AWS.DynamoDB(params);
 }
@@ -22,25 +22,25 @@ DynamoDriver.prototype.collection = function(tableName) {
 
 DynamoDriver.prototype.createTable = function(tableName, extraParams) {
   var that = this;
-  
+
   var params = {
     TableName: tableName,
     KeySchema:            [{ AttributeName: '_id', KeyType: 'HASH' }],
     AttributeDefinitions: [{ AttributeName: '_id', AttributeType: 'S' }],
     ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
   };
-  
+
   if (typeof params !== 'undefined') {
     _.merge(params, extraParams);
   }
-  
+
   return new Promise(function (resolve, reject) {
-    
+
     that.$db.createTable(params, function (err, data) {
       if (err) reject(err);
       else resolve(data);
     });
-    
+
   });
 };
 
@@ -64,12 +64,12 @@ function Adapter(db, tableName) {
 /*
 Adapter.prototype.findFiltered = function(expression, attributeValues) {
   return new Promise(function (resolve, reject) {
-    
+
     this.$db.query({
       TableName: this.$tableName,
       AttributesToGet: projection,
     })
-    
+
   });
 };
 */
@@ -77,16 +77,16 @@ Adapter.prototype.findFiltered = function(expression, attributeValues) {
 
 Adapter.prototype.scan = function(filter, projection, limit) {
   assert.equal(typeof filter, 'function', 'First argument to Adapter#scan should be a function, got ' + typeof filter);
-  
+
   var that = this;
   var params = {
     TableName: this.$tableName
   };
-  
+
   if (typeof projection !== 'undefined') { params.AttributesToGet = projection; }
   if (typeof limit !== 'undefined') { params.Limit = limit; }
-  
-  
+
+
 
   var itemsLeft = true;
   var lastEvaluatedKey = null;
@@ -129,81 +129,81 @@ Adapter.prototype.scan = function(filter, projection, limit) {
     });
 
   });
-  
+
 };
 
 
 Adapter.prototype.query = function(params_to_merge) {
   assert.equal(typeof params_to_merge, 'object', 'First argument to Adapter#query should be an object, got ' + typeof params_to_merge);
-  
+
   var that = this;
   var params = {
     TableName: this.$tableName
   };
-  
+
   _.merge(params, params_to_merge);
   debug('Adapter#query with params: %s', require('util').inspect(params, { depth: 5 }));
-  
+
   return new Promise(function (resolve, reject) {
     that.$db.query(params, function (err, data) {
       if (err) reject(err);
       else resolve(_.map(data.Items, _unpackTypes));
     });
   });
-  
+
 };
 
 
 Adapter.prototype.find = function(query, projection, limit) {
   assert.equal(typeof query, 'object', 'Parameter should be an object. Got ' + typeof query);
-    
+
   if (_.isEqual(query, {})) {
     debug('Using Adapter#scan because query is empty');
     return this.scan(_.identity, projection, limit);
   }
-  
+
   // var filterFn = query.filterFn;
   // try { delete query.filterFn; } catch (e) {}
-  
+
   var cond = {};
   Object.keys(query).forEach(function (index) {
     // - ff string assume equals
     if (typeof query[index] === 'string') {
       cond[index] = { ComparisonOperator: 'EQ', AttributeValueList: [{ S: query[index] }] };
-      
+
     } else if (typeof query[index] === 'object') {
-      
+
       assert.equal(Object.keys(query[index]).length, 1, 'At this time, only exactly one ComparisonOperator is supported at a time.');
-      
+
       if (query[index].hasOwnProperty('$gt')) {
         cond[index] = { ComparisonOperator: 'GT', AttributeValueList: [{ S: query[index] }] };
-        
+
       } else if (query[index].hasOwnProperty('$ge')) {
         cond[index] = { ComparisonOperator: 'GE', AttributeValueList: [{ S: query[index] }] };
-        
+
       } else if (query[index].hasOwnProperty('$lt')) {
         cond[index] = { ComparisonOperator: 'LT', AttributeValueList: [{ S: query[index] }] };
-        
+
       } else if (query[index].hasOwnProperty('$le')) {
         cond[index] = { ComparisonOperator: 'LE', AttributeValueList: [{ S: query[index] }] };
-        
+
       } else if (query[index].hasOwnProperty('$in')) {
         cond[index] = { ComparisonOperator: 'IN', AttributeValueList: [{ SS: query[index] }] };
-        
+
       } else {
         throw new Error('Unsupported ComparisonOperator: ' + Object.keys(query[index]));
       }
-      
+
     } else {
       throw new Error('Unsupported query attribute type: ' + typeof query[index]);
     }
-    
+
   });
-  
-  
+
+
   debug('Built Condition: %s', _str(cond));
   var adapter = this;
-  
+
   /*adapter.$db.createTable({
     TableName: 'events',
     KeySchema: [ // The type of of schema.  Must start with a HASH type, with an optional second RANGE.
@@ -219,8 +219,8 @@ Adapter.prototype.find = function(query, projection, limit) {
         }
     ],
     ProvisionedThroughput: { // required provisioned throughput for the table
-        ReadCapacityUnits: 1, 
-        WriteCapacityUnits: 1, 
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1,
     }}, function (err, data) {
     console.log(arguments);
   });
@@ -228,29 +228,29 @@ Adapter.prototype.find = function(query, projection, limit) {
   adapter.$db.listTables({ Limit: 100 }, function () { console.log(arguments); });
   adapter.$db.scan({ TableName: this.$tableName }, function () { console.log('SCAN', _str(arguments)); });
   */
-  
+
   var params = {
     TableName: adapter.$tableName,
     KeyConditions: cond,
     Limit: limit
   };
-  
+
   if (typeof projection !== 'undefined') { params.AttributesToGet = projection; }
   if (typeof limit !== 'undefined') { params.Limit = limit; }
-  
+
   return new Promise(function (resolve, reject) {
     adapter.$db.query(params, function (err, data) {
       if (err) reject(err);
       else resolve(_.map(data.Items, _unpackTypes));
     });
-    
+
   });
 };
 
 
 Adapter.prototype.findOne = function(query, project) {
   // assertHashRange(query);
-  
+
   return new Promise(function (resolve, reject) {
     this.find(query, project, 1).then(function (rows) {
       resolve(rows[0]);
@@ -261,11 +261,11 @@ Adapter.prototype.findOne = function(query, project) {
 
 Adapter.prototype.save = function(doc) {
   var that = this;
-  
+
   if (!doc._id) {
     doc._id = shortId.generate();
   }
-  
+
   var params = {
     TableName: this.$tableName,
     Key: {
@@ -273,25 +273,25 @@ Adapter.prototype.save = function(doc) {
     },
     AttributeUpdates: {}
   };
-  
+
   _.keys(doc).forEach(function (attrName) {
     if (attrName === '_id') return;
-    
+
     params.AttributeUpdates[attrName] = {
       Action: 'PUT',
       Value: { S: doc[attrName] + '' }
     };
   });
-  
+
   debug('Insert %s', _str(params));
-  
+
   return new Promise(function (resolve, reject) {
     that.$db.updateItem(params, function (err) {
       if (err) reject(err);
       else resolve();
     });
   });
-  
+
 };
 
 
@@ -301,15 +301,15 @@ Adapter.prototype.save = function(doc) {
 
 function assertHashRange(obj) {
   assert.equal(typeof obj, 'object', 'Parameter should be an object. Got ' + typeof obj);
-  
+
   if (!obj.hasOwnProperty('hash')) {
     obj.hash = null;
   }
-  
+
   if (!obj.hasOwnProperty('range')) {
     obj.range = null;
   }
-  
+
   var ok = _.isEqual(_.keys(obj), ['hash', 'range']);
   assert(ok, 'Object can only have a hash and/or a range property.');
 }
@@ -323,3 +323,6 @@ function _unpackTypes(obj) {
 function _str(obj) {
   return util.inspect(obj, { depth: 5 });
 }
+
+
+module.exports = DynamoDriver;
